8106f0798243ff8a090178168a4115e596d00851
Athirat Marasri <athiratwork21@gmail.com>
2026-08-23 19:37:30 +0700

    ข้อความอธิบายงาน

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development', // ปิด PWA ตอนรันแบบ dev จะได้ไม่รวน
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -8,4 +17,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
